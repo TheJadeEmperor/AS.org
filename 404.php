070506b03ec9969edde8822056342f7b932e7b33
TheJadeEmperor <benjamin_louie@drugfree.org>
2019-10-28 15:29:23 -0400

    add 404 page

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php
+/* Template Name: Info Drinking 1 */ 
+
+get_header();
+?>
+<div class="main_container">
+<p>&nbsp;</p><p>&nbsp;</p>
+<h1>Page Not Found</h1>
+
+<p>&nbsp;</p>
+<p>Please check the URL and try again <br />
+or return to the <a href="./">homepage</a></p>
+
+<p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p>
+</div>
+
+<?php
+
+$footer_404_page = 'footer_404_page';
+
+get_footer();
+?>
